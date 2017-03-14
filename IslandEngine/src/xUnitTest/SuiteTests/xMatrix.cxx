b93360043e6bb1c8353db05937df3c6f1f44f16d
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-03-15 00:24:16 +0300

    math::Matrix class new unit test' suite was added.

--- IslandEngine/src/xUnitTest/SuiteTests/xMatrix.cxx
+++ IslandEngine/src/xUnitTest/SuiteTests/xMatrix.cxx
@@ -22,18 +22,27 @@ UNIT_SUITE_CLASS(isle::math::Matrix)
         Matrix const m1(vec[0], vec[1], vec[02], vec[03], vec[04], vec[05], vec[06], vec[07],
                         vec[8], vec[9], vec[10], vec[11], vec[12], vec[13], vec[14], vec[15]);
 
-        Matrix const m2(vec);
-
-        Matrix const m3(m2);
-
         for (auto i = 0; i < 16; ++i)
             CHECK_EQUAL(vec[i], m1.vec_[i], "Matrix(float, float, ...)");
+
+        Matrix const m2(vec);
+        Matrix const m3(std::array<float, 16>{vec[0], vec[1], vec[02], vec[03], vec[04], vec[05], vec[06], vec[07],
+                         vec[8], vec[9], vec[10], vec[11], vec[12], vec[13], vec[14], vec[15]});
 
         for (auto i = 0; i < 16; ++i)
             CHECK_EQUAL(vec[i], m2.vec_[i], "Matrix(std::array<float, 16> const &)");
 
         for (auto i = 0; i < 16; ++i)
-            CHECK_EQUAL(m2.vec_[i], m3.vec_[i], "Matrix(Matrix const &)");
+            CHECK_EQUAL(vec[i], m3.vec_[i], "Matrix(std::array<float, 16> &&)");
+
+        Matrix const m4(m2);
+        Matrix const m5(std::move(Matrix(vec)));
+
+        for (auto i = 0; i < 16; ++i)
+            CHECK_EQUAL(vec[i], m4.vec_[i], "Matrix(Matrix const &)");
+
+        for (auto i = 0; i < 16; ++i)
+            CHECK_EQUAL(vec[i], m5.vec_[i], "Matrix(Matrix &&)");
     }
 
     // Getters...
@@ -110,8 +119,13 @@ UNIT_SUITE_CLASS(isle::math::Matrix)
 
         Matrix m1;
 
-        for(auto i = 0; i < 16; ++i)
-            CHECK_EQUAL(m0.m()[i], (m1 = m0).m()[i], "operator= (Matrix const &)")
+        for (auto i = 0; i < 16; ++i)
+            CHECK_EQUAL(m0.m()[i], (m1 = m0).m()[i], "operator= (Matrix const &)");
+
+        m1 = m1;
+
+        for (auto i = 0; i < 16; ++i)
+            CHECK_EQUAL(m0.m()[i], (m1 = m0).m()[i], "operator= (Matrix const &)");
     }
 
     // The bool operators...
@@ -214,7 +228,7 @@ UNIT_SUITE_CLASS(isle::math::Matrix)
 
         Matrix m(vec);
 
-        CHECK(identity == Matrix::GetIdentity(), "GetIdentity()");
+        CHECK(identity == Matrix::Identity(), "Identity()");
         CHECK(identity == m.MakeIdentity(), "MakeIdentity()");
     }
 
@@ -246,14 +260,14 @@ UNIT_SUITE_CLASS(isle::math::Matrix)
 
         Matrix const m(vec);
 
-        CHECK(Matrix::GetIdentity() == Matrix::GetIdentity() * Matrix::GetIdentity(), "Matrix operator* (Matrix const &) const");
+        CHECK(Matrix::Identity() == Matrix::Identity() * Matrix::Identity(), "Matrix operator* (Matrix const &) const");
 
-        CHECK(m == Matrix::GetIdentity() * m, "Matrix operator* (Matrix const &) const");
-        CHECK(m == m * Matrix::GetIdentity(), "Matrix operator* (Matrix const &) const");
+        CHECK(m == Matrix::Identity() * m, "Matrix operator* (Matrix const &) const");
+        CHECK(m == m * Matrix::Identity(), "Matrix operator* (Matrix const &) const");
 
         Matrix const m0(
             +10.f, 20.f, 30.f, 40.f,
-            -6.f, 20.f,  46.f, 72.f,
+            -6.f,  20.f, 46.f, 72.f,
             -22.f, 20.f, 62.f, 104.f,
             -38.f, 20.f, 78.f, 136.f
         );
@@ -273,16 +287,47 @@ UNIT_SUITE_CLASS(isle::math::Matrix)
         CHECK(m0 != (m2 *= m3), "operator*= (Matrix const &)");
     }
 
+    // The matrix translation routines...
+    /*{
+
+        CHECK(saved == vec, "TransformPosition(Vector &) const");
+    }*/
+
+    // The transform position by matrix routines...
+    {
+        std::array<float, 3> vec{rand<float>(), rand<float>(), rand<float>()};
+
+        Vector v(vec);
+        auto const saved = v;
+
+        auto m = Matrix::Identity();
+        m.TransformPosition(v);
+
+        CHECK(saved == v, "TransformPosition(Vector &) const");
+
+        m.Translate(-vec[0], -vec[1], -vec[2]);
+        m.TransformPosition(v);
+
+        CHECK(Vector(0, 0, 0) == v, "TransformPosition(Vector &) const");
+        CHECK(Vector(0, 0, 0) == m.TransformPosition(saved), "TransformPosition(Vector &) const");
+        CHECK(Vector(0, 0, 0) == m.TransformPosition(Vector(vec)), "TransformPosition(Vector &&) const");
+
+        m = Matrix::Identity();
+        m.RotateOnOZ(90.f);
+
+        //CHECK(Vector(-1, 0, 0) == m.TransformPosition(Vector(0, 1, 0)), "TransformPosition(Vector &&) const");
+    }
+
     // The matrix inverse routines...
     /*{
-        std::array<float, 16> vec;
-        for (auto &a : vec)
-            a = rand<float>();
+    std::array<float, 16> vec;
+    for (auto &a : vec)
+    a = rand<float>();
 
-        Matrix const m(vec);
+    Matrix const m(vec);
 
-        auto transposed = Matrix::Transpose(m);
+    auto transposed = Matrix::Transpose(m);
 
-        CHECK(Matrix::GetIdentity() == transposed * m, "Matrix operator* (Matrix const &) const");
+    CHECK(Matrix::Identity() == transposed * m, "Matrix operator* (Matrix const &) const");
     }*/
 }
