e7e45db2340f0f3fe1d52c8e75187c8729eecaf4
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-04-30 18:06:01 +0300

    Instancing shader file added.

---
+++ contents/shaders/Defaults/Unlit-Instanced.glsl
@@ -0,0 +1,30 @@
+/****************************************************************************************
+****
+****    Source code of Island Engine.
+****    Copyright (C) 2009 - 2017 Crusoe's Island LLC.
+****
+****    Description: default instancing shader file.
+****
+****************************************************************************************/
+#pragma include("Includes/ShaderVariables.glsl")
+#pragma include("Includes/ShaderHelpers.glsl")
+
+#if CRUS_VERTEX_SHADER
+
+layout(location = nVERTEX) in vec3 inVertex;
+
+void main()
+{
+    gl_Position = TransformInstancedFromModelToClip(inVertex);
+}
+
+#elif CRUS_FRAGMENT_SHADER
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0);
+}
+
+#endif
