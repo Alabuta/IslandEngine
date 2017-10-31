d335f68a52fc80f7f911bca14acf0ec1b19282e7
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-10-31 23:55:23 +0300

    Full screen quad render shader added.

---
+++ contents/shaders/Defaults/Fullscreen-Quad.glsl
@@ -0,0 +1,40 @@
+/********************************************************************************************************************************
+****
+****    Source code of Island Engine.
+****    Copyright (C) 2009 - 2017 Crusoe's Island LLC.
+****
+****    Description: http://www.dhpoware.com/demos/gl3FullScreenQuad.html
+****
+********************************************************************************************************************************/
+
+#pragma stage("vertex")
+
+layout(location = nVERTEX) in vec2 inVertex;
+
+out vec2 texCoord;
+
+void main()
+{
+    // inVertex is in normalized device coordinates (NDC).
+    // The (x, y) coordinates are in the range [-1, 1].
+    gl_Position = vec4(inVertex, 0.0, 1.0);
+
+    // To generate the texture coordinates for the resulting full screen
+    // quad we need to transform the vertex position's coordinates from the
+    // [-1, 1] range to the [0, 1] range. This is achieved with a scale and
+    // a bias.
+    texCoord = inVertex * 0.5 + 0.5;
+}
+
+#pragma stage("fragment")
+
+layout(early_fragment_tests) in;
+
+layout(location = nFRAG_COLOR/*, index = 0*/) out vec4 FragColor;
+
+in vec2 texCoord;
+
+void main()
+{
+    FragColor = vec4(texCoord.xy, 0, 1);
+}
