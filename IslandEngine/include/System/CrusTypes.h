561e6bfe96f904cc12537e8c10d8b8df232968e7
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-10-21 12:59:20 +0300

    New helper routines.

--- IslandEngine/include/System/CrusTypes.h
+++ IslandEngine/include/System/CrusTypes.h
@@ -47,17 +47,6 @@ bool IsEqualBasedULP(float a, float b, float max_diff, i32 ulp_diff);
 
 
 namespace isle {
-template<class C, class = void>
-struct is_printable : std::false_type { };
-
-template<class C>
-struct is_printable<C, std::void_t<decltype(std::cout << std::declval<C>())>> : std::true_type { };
-
-/*template<class T>
-constexpr bool is_printable_v = is_printable<T>::value;*/
-
-template<class T>
-constexpr bool is_printable_t = std::is_same_v<is_printable<T>, std::true_type>;
 
 template<class C, class = void>
 struct is_iterable : std::false_type { };
@@ -80,12 +69,20 @@ constexpr bool is_container_v = is_container<C>::value;
 template<class T> struct always_false : std::false_type { };
 
 template<class T, class... Ts>
-struct are_same_types {
+struct are_same {
     static auto constexpr value_type = std::conjunction_v<std::is_same<T, std::decay_t<Ts>>...>;
 };
 
 template<class T, class... Ts>
-inline auto constexpr are_same_types_v = are_same_types<T, Ts...>::value_type;
+inline auto constexpr are_same_v = are_same<T, Ts...>::value_type;
+
+template<class T, class... Ts>
+struct is_one_of {
+    static auto constexpr value = std::disjunction_v<std::is_same<T, Ts>...>;
+};
+
+template<class T, class... Ts>
+auto constexpr is_one_of_v = is_one_of<T, Ts...>::value;
 
 //template<class T, typename std::enable_if_t<std::is_integral_v<std::decay_t<T>>>...>
 constexpr std::uint16_t operator"" _ui16(unsigned long long value)
@@ -102,18 +99,27 @@ constexpr void set_tuple(T &&tuple, V value)
         set_tuple<i + 1>(std::forward<T>(tuple), value);
 }
 
-
-template<class... Ts>
-constexpr std::array<std::decay_t<std::tuple_element_t<0, std::tuple<Ts...>>>, sizeof...(Ts)> make_array(Ts &&...t)
+namespace detail
 {
-    return {{std::forward<Ts>(t)...}};
+template<class T, class... Ts> struct return_type_helper { using type = T; };
+template<class... Ts> struct return_type_helper<void, Ts...> : std::common_type<Ts...> { };
+
+template<class T, class... Ts>
+using return_type_helper_t = typename return_type_helper<T, Ts...>::type;
 }
 
-namespace detail {
+template<class T = void, class... Ts>
+constexpr std::array<detail::return_type_helper_t<T, Ts...>, sizeof...(Ts)> make_array(Ts &&...t)
+{
+    return {{ std::forward<Ts>(t)... }};
+}
+
+namespace detail
+{
 template <class T, std::size_t N, std::size_t... I>
 constexpr std::array<std::remove_cv_t<T>, N> to_array_impl(T(&a)[N], std::index_sequence<I...>)
 {
-    return {{a[I]...}};
+    return {{ a[I]... }};
 }
 }
 
@@ -122,6 +128,36 @@ constexpr std::array<std::remove_cv_t<T>, N> to_array(T(&a)[N])
 {
     return detail::to_array_impl(a, std::make_index_sequence<N>{});
 }
+
+template<class T, std::size_t N = 1>
+struct instances_number {
+    using type = T;
+    static auto constexpr number = N;
+};
+
+template<class T, class Tuple, std::size_t I = 0>
+constexpr std::size_t get_type_instances_number()
+{
+    using E = std::tuple_element_t<I, Tuple>;
+
+    if constexpr (std::is_same_v<T, typename E::type>)
+        return E::number;
+
+    else if constexpr (I + 1 < std::tuple_size_v<Tuple>)
+        return get_type_instances_number<T, Tuple, I + 1>();
+
+    else return 0;
+}
+
+#if _CRUS_TEMP_DISABLED
+template<class V>
+struct wrap_variant_by_vector;
+
+template<class... Ts>
+struct wrap_variant_by_vector<std::variant<Ts...>> {
+    using type = std::variant<std::vector<Ts>...>;
+};
+#endif
 
 #if _CRUS_TEMP_DISABLED
 template<class C, std::enable_if_t<!is_printable_t<std::decay_t<C>> && is_iterable_t<std::decay_t<C>>>...>
