6378d2d85a0dbf4396b69b9540a8ccf305f9eaa3
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-11-28 22:23:37 +0200

    [WIP] Mesh and submesh.

---
+++ IslandEngine/src/mesh.hxx
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+
+#include <GL/GLcoreARB.h>
+
+
+#include "System/CrusIsland.h"
+#include "System/CrusTypes.h"
+
+#include "vertex_format.hxx"
+
+
+namespace isle
+{
+enum class PRIMITIVE_TYPE {
+    NONE,
+    POINTS = GL_POINTS,
+    LINES, LINE_LOOP, LINE_STRIP,
+    TRIANGLES, TRIANGLE_STRIP, TRIANGLE_FAN
+};
+
+namespace staging
+{
+struct submesh {
+    std::size_t begin, end;
+    std::size_t material;
+};
+
+struct mesh {
+    PRIMITIVE_TYPE primitive_type{PRIMITIVE_TYPE::NONE};
+
+    std::size_t primitives_count{0};
+
+    isle::vertex_buffer_t vertices;
+    isle::index_buffer_t indices;
+
+    std::vector<submesh> submeshes;
+};
+
+struct scene_t {
+    std::vector<mesh> meshes;
+};
+}
+}
