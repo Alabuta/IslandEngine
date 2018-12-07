74dfcadd30804193a3377ca92366d213ac747151
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-12-07 16:52:02 +0200

    [WIP] Cross-platform compilation approach.

---
+++ IslandEngine/src/main.cxx
@@ -0,0 +1,90 @@
+#include "config.h"
+
+#include <iostream>
+#include <string>
+#include <string_view>
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+#define GLM_FORCE_CXX17
+#define GLM_ENABLE_EXPERIMENTAL
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/hash.hpp>
+
+#define GL_GLEXT_PROTOTYPES 0
+
+#include <GL/glcorearb.h>
+#include <GL/glext.h>
+
+#ifdef _MSC_VER
+#define WGL_WGLEXT_PROTOTYPES 1
+#include <GL/wglext.h>
+
+// #pragma comment(lib, "OpenGL32.lib")
+#endif
+
+#define GLFW_INCLUDE_GLEXT
+#include <GLFW/glfw3.h>
+
+
+int main()
+try {
+    auto width = 800;
+    auto height = 600;
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 32);
+    glfwWindowHint(GLFW_STENCIL_BITS, 0);
+
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+    glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
+
+    // glfwSetErrorCallback(error_callback);
+
+    auto window = glfwCreateWindow(width, height, "IslandEngine", nullptr, nullptr);
+
+    if (!window) 
+        return 1;
+
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    glClearColor(.5f, .5f, .5f, 1.f);
+
+    while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+        glfwGetFramebufferSize(window, &width, &height);
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+
+} catch (std::exception const &ex) {
+    std::cout << ex.what() << std::endl;
+    std::cin.get();
+}
