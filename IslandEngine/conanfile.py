838e35976bb6e3b82564dabbcea8c5ab9cfe3f22
Almaz Kinziabulatov <auqolaq@gmail.com>
2018-12-18 17:03:41 +0200

    [WIP] Conan based build logic.

--- IslandEngine/conanfile.py
+++ IslandEngine/conanfile.py
@@ -25,10 +25,80 @@ class IslandEngineConan(ConanFile):
         cmake.configure()
         cmake.build()
 
-    def package(self):
-        self.copy(self.name+'*', dst=dstFolder)
-
     def imports(self):
         self.copy('*.dll', dst=dstFolder, src='bin')
         self.copy('*.dylib*', dst=dstFolder, src='lib')
-        self.copy('*.so*', dst=dstFolder, src='lib')
+        self.copy('*.so*', dst=dstFolder, src='lib', excludes=[
+            '*boost_atomic*',
+            '*boost_chrono*',
+            '*boost_container*',
+            '*boost_context*',
+            '*boost_contract*',
+            '*boost_coroutine*',
+            '*boost_date_time*',
+            '*boost_fiber*',
+            '*boost_filesystem*',
+            '*boost_graph*',
+            '*boost_iostreams*',
+            '*boost_locale*',
+            '*boost_log_setup*',
+            '*boost_log*',
+            '*boost_math_c99*',
+            '*boost_math_tr1*',
+            '*boost_prg_exec_monitor*',
+            '*boost_program_options*',
+            '*boost_random*',
+            '*boost_regex*',
+            '*boost_serialization*',
+            '*boost_stacktrace_addr2line*',
+            '*boost_stacktrace_backtrace*',
+            '*boost_stacktrace_basic*',
+            '*boost_stacktrace_noop*',
+            '*boost_system*',
+            '*boost_thread*',
+            '*boost_timer*',
+            '*boost_type_erasure*',
+            '*boost_unit_test_framework*',
+            '*boost_wave*',
+            '*boost_wserialization*'
+        ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
